54ddf6f081388dded7f592548ebdfd6bfcb78df4
Kim SuBin <sb2504@naver.com>
2023-03-28 16:35:24 +0900

    [Feat] 🚧 vuepress 구성 파일 생성 중

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: "Subin's TIL🐰",
+  description: "Subin's Today I Learned",
+  base: "/TIL/",
+  head: [["link", { rel: "icon", href: "/images/ favicon.ico" }]],
+  themeConfig: {
+    nav: [
+      { text: 'Github', link: 'https://github.com/Kim-SuBin' },
+      { text: 'Blog', link: 'https://subin-0320.tistory.com/' }
+    ]
+  }
+};
