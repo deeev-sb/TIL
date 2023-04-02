4dc61bc45b2b90a8190cec1ae4ac9fd341a0c9a1
Kim-SuBin <sb2504@naver.com>
2023-04-02 10:33:34 +0900

    [Comment] 🔇 log 제거

--- docs/.vuepress/sidebar-generator.js
+++ docs/.vuepress/sidebar-generator.js
@@ -2,11 +2,9 @@ const path = require('path');
 const fs = require('fs');
 
 const getSidebarGroup = (targetDir = "", title = '', isCollapsable = true) => {
-    console.log('path.join("docs", dir) = ', path.join('docs', targetDir));
     const workingDir = 'docs';
     const dirAllPath = path.join(workingDir, targetDir);
     const files = getFiles(dirAllPath); // 디렉토리 읽어옴
-    console.log('files = ', files);
 
     return {
         title: title || targetDir,
