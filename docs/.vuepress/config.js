51836b743f2b0683d66e1622360224171fcff61a
Kim SuBin <sb2504@naver.com>
2023-04-03 13:42:17 +0900

    [Chore] 💬ELK 스택으로 데이터 분석 제목 내 ver 추가

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -28,7 +28,7 @@ module.exports = {
             sidebar.getSidebarGroup('springBasic', '[강의] 스프링 핵심 원리 - 기본편'),
             sidebar.getSidebarGroup('springInAction', '[책] 스프링 인 액션'),
             sidebar.getSidebarGroup('java8', '[강의] 더 자바, Java 8'),
-            sidebar.getSidebarGroup('analyze-data-with-elk-stack', '[강의] ELK 스택으로 데이터 분석'),
+            sidebar.getSidebarGroup('analyze-data-with-elk-stack', '[강의] ELK 스택으로 데이터 분석 (7.10.2 ver)'),
             sidebar.getSidebarGroup('docker&k8s', '[책] 그림과 실습으로 배우는 도커 & 쿠버네티스'),
         ],
     }
